6a8ca8438d38b304b2c545c103126214be67983f
Jasmeet Samra <js.samra@outlook.com>
2023-05-25 13:48:56 +0200

    Add Assosiation and Validation for list movie bookmars models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ApplicationRecord
-  belongs_to :bookmark
+  has_many :bookmarks
+
+  # validates :title, presence: true, uniqueness: true
+  validates :title, :overview, presence: true
+  validates :title, uniqueness: true
 end
